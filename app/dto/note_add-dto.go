5718eb4d50cf90c68e2d417037f5d33d5836dc48
Hajar Aswad <hajaraswadkom@gmail.com>
2022-07-27 06:33:40 +0700

    remove required desc

--- app/dto/note_add-dto.go
+++ app/dto/note_add-dto.go
@@ -3,6 +3,6 @@ package dto
 type NoteAddDto struct {
 	Title       string `json:"title" form:"title" binding:"required,min=3,max=100"`
 	Body        string `json:"body" form:"body" binding:"required,min=3"`
-	Description string `json:"desc" form:"description" binding:"required,min=3"`
+	Description string `json:"desc" form:"description"`
 	UserID      uint64
 }
